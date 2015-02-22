dbb8d771fc2f7d4dcfb84471556fcb057140b642
Albert Callarisa <shark234@gmail.com>
2015-02-22 18:48:14 +0100

    Ants don't go away

--- sim/ant.go
+++ sim/ant.go
@@ -23,7 +23,7 @@ func NewAnt(position *util.Point) *Ant {
 	go func() {
 		for {
 			ant.Move()
-			time.Sleep(50 * time.Millisecond)
+			time.Sleep(25 * time.Millisecond)
 		}
 	}()
 	return ant
@@ -32,6 +32,18 @@ func NewAnt(position *util.Point) *Ant {
 // Move implements the ant movement
 func (ant *Ant) Move() {
 	ant.Position.X += math.Cos(ant.Direction.Angle) / 1000
+	if ant.Position.X > 1 {
+		ant.Position.X--
+	}
+	if ant.Position.X < 0 {
+		ant.Position.X++
+	}
 	ant.Position.Y += math.Sin(ant.Direction.Angle) / 1000
+	if ant.Position.Y > 1 {
+		ant.Position.Y--
+	}
+	if ant.Position.Y < 0 {
+		ant.Position.Y++
+	}
 	ant.Direction.Angle += (rand.Float64() - 0.5) / 4
 }
