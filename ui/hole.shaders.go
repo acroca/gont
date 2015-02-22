632e279ae79386c97da005c26439e56a633b0cbe
Albert Callarisa <shark234@gmail.com>
2015-02-23 00:34:19 +0100

    Circle hole

--- ui/hole.shaders.go
+++ ui/hole.shaders.go
@@ -12,20 +12,15 @@ const (
 	holeG = `
     #version 400
     layout(points) in;
-    layout(triangle_strip, max_vertices = 5) out;
+    layout(line_strip, max_vertices = 11) out;
 
     void main() {
-      gl_Position = (gl_in[0].gl_Position + vec4(-0.02, -0.02, 0.0, 0.0));
-      EmitVertex();
-      gl_Position = (gl_in[0].gl_Position + vec4(-0.02,  0.02, 0.0, 0.0));
-      EmitVertex();
-      gl_Position = (gl_in[0].gl_Position + vec4( 0.02,  0.02, 0.0, 0.0));
-      EmitVertex();
-      gl_Position = (gl_in[0].gl_Position + vec4( 0.02, -0.02, 0.0, 0.0));
-      EmitVertex();
-      gl_Position = (gl_in[0].gl_Position + vec4(-0.02, -0.02, 0.0, 0.0));
-      EmitVertex();
-
+			int lines = 10;
+			float step = 6.38 / lines;
+			for(float i = 0; i <= 6.38 ; i+=step){
+	      gl_Position = (gl_in[0].gl_Position + vec4(0.02 * cos(i), 0.02 * sin(i), 0.0, 0.0));
+	      EmitVertex();
+			}
       EndPrimitive();
     }`
 	holeF = `
