0b7c499428f6697ea4890ab23d59e6cc6fc404b6
Albert Callarisa <shark234@gmail.com>
2015-02-23 22:59:14 +0100

    flag for the amount of ants

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/acroca/gont/sim"
 	"github.com/acroca/gont/ui"
 )
 
+var numAnts = flag.Int("ants", 1, "Number of ants in the simulator")
+
 func main() {
-	world := sim.NewWorld(1)
+	flag.Parse()
+	world := sim.NewWorld(*numAnts)
 	window := ui.NewWindow(world)
 	go world.Start()
 	window.Open()
